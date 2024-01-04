88f85eeb192751bdeffc3558b4081bcafc8579bc
KifilweJasmin <137528030+KifilweJasmin@users.noreply.github.com>
2024-01-05 00:06:25 +0200

    Added the GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">weather app</div>;
+  return (
+    <div className="App">
+      <h1> weather app</h1>
+      <footer>
+        This project was coded by KifilweJasmin and is{" "}
+        <a
+          href="https://github.com/KifilweJasmin/weather-react--2"
+          target="blank"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
